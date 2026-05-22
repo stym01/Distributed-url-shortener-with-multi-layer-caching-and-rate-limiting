c3e74546b05641f35bdca19fee550db9dffe8278
Satyam Kesharwani <satyamkesharwani134@gmail.com>
2026-05-22 22:08:21 +0530

    rate limiter using token bucket added

--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { createShortUrl, redirectUrl } from '../controllers/url.controller.js';
+import { tokenBucketLimiter } from '../middleware/rateLimiter.js';
 
 const router = express.Router();
 
-router.post('/shorten', createShortUrl);
-router.get('/:shortCode', redirectUrl);
+// Limit URL creation
+router.post('/shorten', tokenBucketLimiter, createShortUrl);
+
+// Limit URL redirects
+router.get('/:shortCode', tokenBucketLimiter, redirectUrl);
 
 export default router;
